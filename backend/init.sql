59cf7c25baaade4c7b145b4f771f4e051e0d513f
paulazf <paulavzf2004@gmail.com>
2026-04-25 02:50:53 +0000

    chore(database): actualiza estructura de tabla members y estado de integrantes

--- backend/init.sql
+++ backend/init.sql
@@ -5,12 +5,12 @@ CREATE TABLE members (
 	legajo int,
 	feature varchar(10),
 	servicio varchar(30),
-	estado varchar(20) check (estado in ('Activo', 'Inactivo'))
+	estado varchar(20)
 );
 
 INSERT INTO members (nombre, apellido, legajo, feature, servicio, estado)
-VALUES  ('Rodrigo', 'Castaño', 33659, '02', 'Frontend', 'Activo'),
-        ('Conrado', 'Cemino', 32058, '01', 'Coordinación', 'Activo'),
-        ('Matías', 'Cortés', 31966, '05', 'Portainer', 'Activo'),
-        ('Ángeles', 'Schneeberger', 33589, '03', 'Backend', 'Activo'),
-        ('Paula', 'Zacarías', 33638, '04', 'Database', 'Activo');
+VALUES  ('Rodrigo', 'Castaño', 33659, '02', 'Frontend', 'Completado'),
+        ('Conrado', 'Cemino', 32058, '01', 'Coordinación', 'Completado'),
+        ('Matías', 'Cortés', 31966, '05', 'Portainer', 'Completado'),
+        ('Ángeles', 'Schneeberger', 33589, '03', 'Backend', 'Completado'),
+        ('Paula', 'Zacarías', 33638, '04', 'Database', 'Completado');
